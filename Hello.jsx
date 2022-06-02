6dcba056efd8f94510b56009d336fb7672e39771
jewonjeong <104977571+jewonjeong@users.noreply.github.com>
2022-06-02 20:35:16 +0900

    react

---
+++ Hello.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hello.css"
+function Hello(){
+    const PracticeStyle = {
+        marginTop:"10px",
+        backgroundColor:"blue"
+    };
+    return(
+    <>
+    <div style={{marginTop:"10px", backgroundColor:"red" }}>
+        Hello World!
+    </div>
+    </>
+    );
+}
+export default Hello;
